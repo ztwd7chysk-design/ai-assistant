d1a688e07e806bd87cf355a84087bb683d1f27e2
yehy070805 <yehy070805@gmail.com>
2026-06-29 11:44:23 +0800

    Day1:python基础 -print 变量 input f-string 练习

---
+++ day1.py
@@ -0,0 +1,19 @@
+'''
+name = input("你好！你叫什么名字呀？")
+age = input("你今年几岁了？")
+height = input("你有多高啊？")
+print(f"你的名字是：{name}")
+print(f"你的年龄是：{age}")
+print(f"你的身高是：{height}")
+'''
+print("===AI助理问候系统===")
+name = input("你的名字叫什么？")
+age = input("你今年几岁？")
+place = input("你现在在哪个城市？")
+goal = input("你未来的学习目标是什么？")
+
+print(f"你好，{name}!")
+print(f"你今年{age}岁了。")
+print(f"你现在在{place}。")
+print(f"你的学习目标是：{goal}")
+print("我记住你了！")
---
+++ hello.py
@@ -0,0 +1,37 @@
+
+print("Hello, World!")
+print("今天是周一")
+print(123)
+print(3.14)
+# print()能直接打印数字，小数，但是打印字符串需要加上引号
+print("今天学习了python基础")
+print("感觉很有意思")
+
+
+name = "小明"
+age = 18
+height = 178
+
+print(name)
+print(age)
+print(height)
+
+my_name = "yhy"
+my_age = 18
+my_height = 178
+print(my_name)
+print(my_age)
+print(my_height)
+
+Name = input("请输入你的名字:")
+Age = input("请输入你的年龄：")
+Height = input("请输入你的身高：")
+
+print("你的名字是：" + Name)
+print("你的年龄是：" + Age)
+print("你的身高是：" + Height)
+print(f"你好，{Name}")
+#fstring格式化字符串，f后面跟着字符串，字符串中可以直接使用变量
+
+
+
